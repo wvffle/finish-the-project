0adea8e0d0e9f5e1913097608157db2d05fb74c2
Sylwia0506 <sysazeszkoly@gmail.com>
2021-11-15 23:58:36 +0100

    feat: add continue button

--- src/scenes/Test.gd
+++ src/scenes/Test.gd
@@ -1,30 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _process(delta):
 	
 	if $AudioStreamPlayer.playing == false:
 		$AudioStreamPlayer.play()
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
-func _on_newGameButton_mouse_entered():
+func _on_Button_mouse_entered():
 	$ButtonSound.play()
-	pass # Replace with function body.
-
-
-func _on_quitGameButton_mouse_entered():
-	$ButtonSound.play()
-	pass # Replace with function body.
