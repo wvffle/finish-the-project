0d5b4cc06d2cd04ce900f1c15459a7bed9ca8073
Viyaleta Rushnitskaya <violetta.rushnitskaya@gmail.com>
2021-12-10 18:59:39 +0100

    chore: merge in GPZ/finish-the-project from feature/GPZ-34 to master
    Squashed commit of the following:
    
    commit 76f807c295ede83be7184e91f09ad21e25ecade9
    Merge: dbac3d2 1675f5e
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Fri Dec 10 18:49:39 2021 +0100
    
        Merge branch 'master' of https://devtools.wi.pb.edu.pl/bitbucket/scm/gpz/finish-the-project into feature/GPZ-34
    
    commit dbac3d2911e03d31b25b531c3626341496eb2c09
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Fri Dec 10 18:18:06 2021 +0100
    
        fix: wyrównanie tekstu (GPZ-34)
    
    commit 2f2373ddcc7b8d4b9d7b6bcfd7ea9c41b9bdce51
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Thu Dec 9 21:13:03 2021 +0100
    
        fix: wyrównanie tekstów i wartości (GPZ-34)
    
    commit e7b1efc8d2c77b33255281d792dac2718b4dc894
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Thu Dec 9 17:21:51 2021 +0100
    
        fix: zmiana nazw pół statystyk, dodanie nowego polu do wyświetlenia maksymalnego osiągniętego poziomu (GPZ-34)
    
    commit 9b2a9e20010c30c6a82b9998aa5b4e441594ecb3
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Tue Dec 7 20:29:38 2021 +0100
    
        fix: Zmiana koloru napisu
    
    commit 9abeb54731798c853ba7a8808ce9c76cfedea1df
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Tue Dec 7 20:20:32 2021 +0100
    
        fix: GPZ-34
    
    commit 967c2b70005d0fd618d82dfc86fa5d0f3106f11e
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Tue Dec 7 13:50:27 2021 +0100
    
        fix: rename script
    
    commit 8bea4b22070138fa141559ea80bb55a68e8e7d0f
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Tue Dec 7 12:28:14 2021 +0100
    
        Revert "fix names"
    
        This reverts commit 779cb4e8670b18d7febb516560e8dda5de9eabe4.
    
    commit 779cb4e8670b18d7febb516560e8dda5de9eabe4
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Mon Dec 6 23:32:29 2021 +0100
    
        fix names
    
    commit 1576cd9b076b338548c6c14a34b237f3dedf8492
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Mon Dec 6 23:25:53 2021 +0100
    
        fix: nazwa przycisku scoreboardButton, nazwa skryptu do Scoreboard
    
    commit 5e6b8e8f1f2d22d59c2b6c74f4718280418bf0d9
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Mon Dec 6 11:45:07 2021 +0100
    
        fix: ScoreboardButton ze sceny Scoreboard
    
    commit 96b7dd005499586689141af98c5d90da75add786
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Sun Dec 5 21:30:51 2021 +0100
    
        feat: GPZ-34
    
    commit ab84afc5897f9f86ca5299027557336425aa3597
    Author: Wioletta <violetta.rushnickaya@mail.ru>
    Date:   Sun Dec 5 21:12:15 2021 +0100
    
        feat: GPZ-34

--- src/scripts/ui/buttons/rankingButton.gd
+++ src/scripts/ui/buttons/rankingButton.gd
@@ -3,3 +3,4 @@ extends Button
 
 func _on_rankingButton_pressed():
 	get_tree().change_scene("res://src/scenes/Ranking.tscn")
+
---
+++ src/scripts/ui/buttons/scoreboardButton.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_scoreboardButton_pressed():
+	get_tree().change_scene("res://src/scenes/Scoreboard.tscn")
