92fe6d19dded311c394da4f5e01b1ee8318c03c6
patrycjarecko <patrycjarecko00@gmail.com>
2021-12-12 15:33:02 +0100

    fix: manual override of looping options

--- src/scripts/sounds.gd
+++ src/scripts/sounds.gd
@@ -11,6 +11,9 @@ func _ready():
 	var node = global.get_node("/root/BackgroundSounds")
 	mainTheme = node.find_node("MainTheme")
 	button = node.find_node("ButtonSound")
+	
+	mainTheme.stream.set_loop_mode(1)
+	mainTheme.stream.set_loop_end(3032064)
 
 func button_hover():
 	if sounds.soundsCanPlay:
