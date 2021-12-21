a4a89dcc415fc3e31f2b9957d16114e3188b15dd
Filip Rutkowski <Frutkowski80@gmail.com>
2021-12-21 18:38:29 +0100

    feat(sounds): Add get_loop_end()
    Merge in GPZ/finish-the-project from fix/GPZ-105 to master
    
    Squashed commit of the following:
    
    commit 8db73670822660711e8400ac8976d4d7c2519136
    Author: SiwyKot41 <Frutkowski80@gmail.com>
    Date:   Tue Dec 21 17:15:05 2021 +0100
    
        Pull changes from master
    
    commit cad19ae6ded47e7e61f255c982bbefed6a40790b
    Merge: 069d5cb ccc65fb
    Author: SiwyKot41 <Frutkowski80@gmail.com>
    Date:   Tue Dec 21 17:12:16 2021 +0100
    
        Merge branch 'master' into fix/GPZ-105
    
    commit 069d5cb43a741336979ca91b28c21b23055a13a9
    Author: Filip Rutkowski <Frutkowski80@gmail.com>
    Date:   Sun Dec 19 22:40:46 2021 +0100
    
        idk czemu to się samo zawsze zmienia
    
    commit 47bf6c9b39a2624defad61a3cb159ac87b644cdd
    Merge: 4279a04 d8f43cb
    Author: SiwyKot41 <Frutkowski80@gmail.com>
    Date:   Sun Dec 19 22:16:49 2021 +0100
    
        Merge master into fix/GPZ-105
    
    commit 4279a04e17ffc4ab40988ac583b7e78d660e2aed
    Author: Filip Rutkowski <Frutkowski80@gmail.com>
    Date:   Sat Dec 18 15:04:55 2021 +0100
    
        sounds.gd edited online with Bitbucket
    
    commit e8d35137e70a55574956af431c5e2e2d220c3565
    Author: SiwyKot41 <Frutkowski80@gmail.com>
    Date:   Sat Dec 18 14:54:07 2021 +0100
    
        feat(sounds): Add get_loop_end()

--- src/scripts/sounds.gd
+++ src/scripts/sounds.gd
@@ -7,16 +7,13 @@ var mainTheme
 var button
 var soundsCanPlay
 
-
 func _ready():
 	var node = global.get_node("/root/BackgroundSounds")
 	mainTheme = node.find_node("MainTheme")
 	button = node.find_node("ButtonSound")
 	
 	mainTheme.stream.set_loop_mode(1)
-	mainTheme.stream.set_loop_end(3032064)
-
-
+	
 func button_hover():
 	if sounds.soundsCanPlay:
 		sounds.button.play()
